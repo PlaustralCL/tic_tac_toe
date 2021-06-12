adfb4154aefd6d241aecc4190c4f0deffc6d7d0a
PlaustralCL <plaustralcl@gmail.com>
2021-06-12 14:40:16 -0400

    Add reset function, fix initiation
    Had to apply the array directly to the instance variable rather
    than using a constant that is assigned to the instance variable. When
    the constant was used, all objects had the same properties.

--- board.rb
+++ board.rb
@@ -8,28 +8,39 @@ class Board
   ].freeze
 
   def initialize
-    @board = BOARD_TEMPLATE
+    @grid =
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ]
   end
 
   # Updates the board array with the correct marker after a player's turn
-  # @param sub_array [Integer] Identifies which sub array (0, 1, or 2) of @board
+  # @param sub_array [Integer] Identifies which sub array (0, 1, or 2) of @grid
   #   will be updated
   # @param element [Integer] Element of sub_array that will be updated
   # @param marker [String] Marker that will be used in the square
   def update_board(sub_array, element, marker)
-    @board[sub_array][element] = marker
+    @grid[sub_array][element] = marker
   end
 
+  def board_status
+    @grid
+  end
+
+  # Displays board onto the terminal
   # rubocop: disable Metrics/AbcSize
   def show_board
     puts ""
-    puts " #{@board[0][0]} | #{@board[0][1]} | #{@board[0][2]} "
+    puts " #{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]} "
     puts "---+---+---"
-    puts " #{@board[1][0]} | #{@board[1][1]} | #{@board[1][2]} "
+    puts " #{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]} "
     puts "---+---+---"
-    puts " #{@board[2][0]} | #{@board[2][1]} | #{@board[2][2]} "
+    puts " #{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]} "
   end
 
+  # Checks for a winning result by testing each row, column, and diagonal
   def check_result
     # horizontal
     return check_horizontal unless check_horizontal == 'none'
@@ -38,13 +49,24 @@ class Board
     return check_diagonal2 unless check_diagonal2 == 'none'
   end
 
+  # Resets the board back to the starting configuration
+  def reset
+    @grid =
+      [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ]
+    @grid
+  end
+
   protected
 
   def check_horizontal
     (0..2).each do |sub_array|
-      row_array = @board[sub_array].join
-      return 'X' if row_array == 'XXX'
-      return 'O' if row_array == 'OOO'
+      row = @grid[sub_array].join
+      return 'X' if row == 'XXX'
+      return 'O' if row == 'OOO'
 
       next
     end
@@ -53,23 +75,23 @@ class Board
 
   def check_vertical
     (0..2).each do |column|
-      column_array = [@board[column][0], @board[column][1], @board[column][2]].join
-      return 'X' if column_array == 'XXX'
-      return 'O' if column_array == 'OOO'
+      column_string = [@grid[0][column], @grid[1][column], @grid[2][column]].join
+      return 'X' if column_string == 'XXX'
+      return 'O' if column_string == 'OOO'
     end
     'none'
   end
 
   def check_diagonal1
-    diagonal_array = [@board[0][0], @board[1][1], @board[2][2]].join
-    return 'X' if diagonal_array == 'XXX'
-    return 'O' if diagonal_array == 'OOO'
+    diagonal = [@grid[0][0], @grid[1][1], @grid[2][2]].join
+    return 'X' if diagonal == 'XXX'
+    return 'O' if diagonal == 'OOO'
 
     'none'
   end
 
   def check_diagonal2
-    diagonal = [@board[2][0], @board[1][1], @board[0][2]].join
+    diagonal = [@grid[2][0], @grid[1][1], @grid[0][2]].join
     return 'X' if diagonal == 'XXX'
     return 'O' if diagonal == 'OOO'
 
@@ -85,6 +107,13 @@ if $PROGRAM_NAME == __FILE__
     else
       puts "ERROR #{test_name} failed. Expected #{expect}, got #{actual}"
     end
+  end
+
+  def test_new_board
+    boarda = Board.new
+    boarda.show_board
+    boardb = Board.new
+    boardb.show_board
   end
 
   def test_show_board
@@ -110,17 +139,19 @@ if $PROGRAM_NAME == __FILE__
     end
     expect = 'X'
     actual = board2.check_result
+    board2.show_board
     test_results(expect, actual, __method__)
   end
 
   def test_horizontal_winner2
     # test with O
-    board2 = Board.new
+    board92 = Board.new
     (0..2).each do |element|
-      board2.update_board(1, element, 'O')
+      board92.update_board(2, element, 'O')
     end
     expect = 'O'
-    actual = board2.check_result
+    actual = board92.check_result
+    board92.show_board
     test_results(expect, actual, __method__)
   end
 
@@ -131,6 +162,7 @@ if $PROGRAM_NAME == __FILE__
     end
     expect = 'O'
     actual = board3.check_result
+    board3.show_board
     test_results(expect, actual, __method__)
   end
 
@@ -141,29 +173,39 @@ if $PROGRAM_NAME == __FILE__
     end
     expect = 'X'
     actual = board4.check_result
+    board4.show_board
     test_results(expect, actual, __method__)
   end
 
   def test_diagonal2
     board5 = Board.new
-    board5.update_board(0, 2, 'X')
-    board5.update_board(1, 1, 'X')
-    board5.update_board(2, 0, 'X')
+    (0..2).each do |element|
+      board5.update_board(element, 2 - element, 'X')
+    end
     expect = 'X'
     actual = board5.check_result
+    board5.show_board
     test_results(expect, actual, __method__)
   end
-  # vertical winner
 
-  # diagonal winner
+  def test_reset
+    board6 = Board.new
+    board6.update_board(1, 1, 'X')
+    board6.show_board
+    expect = Board.new
+    expect.show_board
+    expect = expect.board_status
+    actual = board6.reset
+    test_results(expect, actual, __method__)
+  end
 
-  # tie
-
+  # call test functions
   test_horizontal_winner1
   test_horizontal_winner2
   test_vertical_winner
   test_diagonal1
   test_diagonal2
-
+  test_reset
+  # test_new_board
 
 end
