6127e1f237d0964cdd2e3e19b6b44e04bb4bafa9
PlaustralCL <plaustralcl@gmail.com>
2021-06-14 20:48:32 -0400

    Add test to write to :marker

--- testing/user_test.rb
+++ testing/user_test.rb
@@ -11,6 +11,14 @@ class UserTest < Test
     actual = player1.name
     assert_equal(expected, actual, __method__)
   end
+
+  def test_store_marker
+    player1 = User.new("Joe")
+    player1.marker = "\e[1;36mX\e[0m"
+    expected = "\e[1;36mX\e[0m"
+    actual = player1.marker
+    assert_equal(expected, actual, __method__)
+  end
 end
 
 test = UserTest.new
