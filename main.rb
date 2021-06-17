fd1d506808cd5ec5479b582b9dae78befe5853e1
PlaustralCL <plaustralcl@gmail.com>
2021-06-16 21:43:12 -0400

    Choose markers

--- main.rb
+++ main.rb
@@ -28,14 +28,55 @@ def coin_flip
   rand(2)
 end
 
+# Welcome
 clear_terminal
-puts "Welcome to Tic Tack Toe"
+puts "Welcome to Tic Tac Toe"
 puts "This game is designed for two human players"
 puts ""
+
+# Names
 players = input_names
-puts "Welcome #{players[0].name} and #{players[1].name}!"
-players[0].choose_first_marker
-puts "#{players[0].name} selected  #{players[0].marker}"
+puts ""
+
+# Choose markers
+first_chooser = coin_flip
+second_chooser = ([0, 1] - [first_chooser]).join.to_i
+puts "Before we begin markers for the board need to be choosen. #{players[first_chooser].name} will go first."
+players[first_chooser].choose_first_marker
+players[second_chooser].marker = (MARKER_CHOICES - [players[first_chooser].marker]).join
+print "#{players[first_chooser].name} selected  #{players[first_chooser].marker}. "
+puts "That means #{players[second_chooser].name} is #{players[second_chooser].marker}"
+print "Press `Enter` to continue "
+gets.chomp
+
+# Play game
+first_chooser = coin_flip
+second_chooser = ([0, 1] - [first_chooser]).join.to_i
+players[0], players[1] = players[first_chooser], players[second_chooser]
+clear_terminal
+board = Board.new
+board.show_board
+print "#{players[0].name} please select a number for your move: "
+move = gets.chomp.to_i
+
+case move
+when 1
+  sub_array = 0
+  element = 0
+when 2
+  sub_array = 0
+  element = 1
+when 3
+  sub_array = 0
+  element = 2
+end
+
+board.update_board(sub_array, element, players[0].marker)
+clear_terminal
+board.show_board
+print "#{players[0].name} please select a number for your move: "
+
+
 
 # if $PROGRAM_NAME == __FILE__
 #   main
