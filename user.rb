3ed96d5ac1e86a14b2d1ac7fdfd2b03e9342a235
PlaustralCL <plaustralcl@gmail.com>
2021-06-18 08:04:28 -0400

    Address rubocop comments

--- user.rb
+++ user.rb
@@ -4,7 +4,7 @@
 class User
   MARKER_CHOICES = ["\e[1;36mX\e[0m", "\e[1;31mO\e[0m"].freeze
 
-  attr_accessor :marker
+  attr_accessor :marker, :short_marker
   attr_reader :name
 
   def initialize(name)
@@ -16,10 +16,11 @@ class User
     loop do
       print "#{@name} please choose '#{MARKER_CHOICES[0]}' or '#{MARKER_CHOICES[1]}': "
       marker = gets.chomp.downcase
-      if %w[x o].include?(marker)
-        return @marker = marker == "x" ? MARKER_CHOICES[0] : MARKER_CHOICES[1]
-      end
+      next unless %w[x o].include?(marker)
+
+      @marker = marker == "x" ? MARKER_CHOICES[0] : MARKER_CHOICES[1]
+      @short_marker = marker
+      return
     end
   end
-
 end
