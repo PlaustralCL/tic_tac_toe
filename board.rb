975fa61b9d8a57b29aa282be378e0c357d77add1
PlaustralCL <plaustralcl@gmail.com>
2021-06-18 21:11:25 -0400

    Indent board

--- board.rb
+++ board.rb
@@ -61,11 +61,11 @@ class Board
   # rubocop: disable Metrics/AbcSize
   def show_board
     puts ""
-    puts " #{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]} "
-    puts "---+---+---"
-    puts " #{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]} "
-    puts "---+---+---"
-    puts " #{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]} "
+    puts "       #{@grid[0][0]} | #{@grid[0][1]} | #{@grid[0][2]} "
+    puts "      ---+---+---"
+    puts "       #{@grid[1][0]} | #{@grid[1][1]} | #{@grid[1][2]} "
+    puts "      ---+---+---"
+    puts "       #{@grid[2][0]} | #{@grid[2][1]} | #{@grid[2][2]} "
     puts ""
   end
 
