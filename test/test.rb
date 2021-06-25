a52252155cf94a02755326830e09213c73ced9a9
PlaustralCL <plaustralcl@gmail.com>
2021-06-24 20:41:32 -0400

    Rename test directory

---
+++ test/test.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative "../color"
+
+# Class for performing basic tests
+# Based on https://testing-for-beginners.rubymonstas.org/testing/classes.html
+class Test
+  def run
+    tests = methods.select { |method| method.to_s.start_with?("test_") }
+    tests.each { |test| send(test) }
+  end
+
+  def assert_true(actual, test_name)
+    assert_equal(true, actual, test_name)
+  end
+
+  def assert_false(actual, test_name)
+    assert_equal(false, actual, test_name)
+  end
+
+  def assert_equal(expected, actual, test_name)
+    if expected == actual
+      puts "#{'Passed'.green} #{test_name}. Actual is '#{actual}' as expected"
+    else
+      puts "#{'Failed'.red} #{test_name}. Expected '#{expected}' but got '#{actual}'"
+    end
+  end
+end
